aaffc07659ba9a4d2994b9fbba504a39130dac8b
Aka <jenny50626000@gmail.com>
2023-09-27 15:29:10 +0800

    [feat] Flow Control test
    Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -16,3 +16,27 @@ console.log(typeof ({} + [])); // string
 console.log({} + {}); // [object object][object object]
 console.log(typeof ({} + {})); // string
 //#endregion
+
+//#region Flow Control
+const age = window.prompt("請輸入年齡");
+console.log("if else:");
+if (age >= 18) {
+  console.log("已成年");
+} else if (age >= 12 && age < 18) {
+  console.log("青少年");
+} else {
+  console.log("小朋友");
+}
+
+console.log("switch:");
+switch (age) {
+  case age >= 18:
+    console.log("已成年");
+    break;
+  case age >= 12 && age < 18:
+    console.log("青少年");
+    break;
+  default:
+    console.log("小朋友");
+}
+//#endregion
