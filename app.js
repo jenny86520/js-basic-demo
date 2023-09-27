2561ced773c265c1ab698e14ca7c2e0fec9d5304
Aka <jenny50626000@gmail.com>
2023-09-27 15:28:27 +0800

    [feat] Type coercion test
    Changes to be committed:
    	modified:   app.js

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 console.log("Hello World");
+
+//#region Type Coercion
+console.log(1 + 2); // 3
+console.log(1 + "2"); // 12
+console.log("Hello" + 123); // Hello123
+console.log("Hello" + true); // Hellotrue
+console.log(123 + true); // 124
+console.log(123 + false); // 123
+console.log([] + []); // 空字串
+console.log(typeof ([] + [])); // string
+console.log([] + {}); // [object object]
+console.log(typeof ([] + {})); // string
+console.log({} + []); // [object object]
+console.log(typeof ({} + [])); // string
+console.log({} + {}); // [object object][object object]
+console.log(typeof ({} + {})); // string
+//#endregion
