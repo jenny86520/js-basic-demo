60db51432f97ac812dd21db00a9825f27f6244f3
Aka <jenny50626000@gmail.com>
2023-09-28 14:56:19 +0800

    [feat] Finished todo-list
    Changes to be committed:
    	modified:   todo-list/app.js

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,1 +1,34 @@
-// 程式碼寫這裡
+document.addEventListener("DOMContentLoaded", () => {
+  const input = document.querySelector("#taskInput");
+  const addBtn = document.querySelector("#addBtn");
+  const list = document.querySelector("ul");
+
+  const createTaskItem = () => {
+    const task = input.value;
+    const todoItem = `<li class="todo-item">
+        <span class="item">${task}</span>
+        <button class="closeBtn">X</button>
+    </li>`;
+
+    list.insertAdjacentHTML("afterbegin", todoItem);
+    input.value = "";
+    input.focus();
+  };
+
+  addBtn.addEventListener("click", () => {
+    createTaskItem();
+  });
+
+  list.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.nodeName === "BUTTON") {
+      target.parentNode.remove();
+    }
+  });
+
+  input.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      createTaskItem();
+    }
+  });
+});
