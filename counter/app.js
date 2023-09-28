f142835402d67976d3f53e400214570ec07a4615
Aka <jenny50626000@gmail.com>
2023-09-28 10:40:28 +0800

    [feat] Finished counter
    Changes to be committed:
    	modified:   counter/app.js

--- counter/app.js
+++ counter/app.js
@@ -1,1 +1,13 @@
-// 程式碼寫在這裡
+document.addEventListener("DOMContentLoaded", () => {
+  const minusBtn = document.querySelector("#minus");
+  const plusBtn = document.querySelector("#plus");
+  const counter = document.querySelector("#counter");
+
+  minusBtn.addEventListener("click", () => {
+    counter.value--;
+  });
+
+  plusBtn.addEventListener("click", () => {
+    counter.value++;
+  });
+});
