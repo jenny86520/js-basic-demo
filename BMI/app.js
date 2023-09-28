9d70872ccd07196b5469868bffa4f769233f7917
Aka <jenny50626000@gmail.com>
2023-09-28 10:59:44 +0800

    [feat] Finished BMI
    Changes to be committed:
    	modified:   BMI/app.js

--- BMI/app.js
+++ BMI/app.js
@@ -1,2 +1,13 @@
-// 程式碼寫在這裡
 // 提示：BMI = 體重(kg) / 身高(m) 平方
+document.addEventListener("DOMContentLoaded", () => {
+  const heightInput = document.querySelector("#bodyHeight");
+  const weightInput = document.querySelector("#bodyWeight");
+  const countBtn = document.querySelector("button");
+  const resultText = document.querySelector("#resultText");
+
+  countBtn.addEventListener("click", () => {
+    let h = heightInput.value / 100;
+    let w = weightInput.value;
+    resultText.textContent = (w / Math.pow(h, 2)).toFixed(2);
+  });
+});
