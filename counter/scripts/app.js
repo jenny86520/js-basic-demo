9bb9044482f73bd1e4b77b3783ad75c094f5e7a2
Aka <jenny50626000@gmail.com>
2023-10-04 14:37:04 +0800

    [feat] Finished counter
    Changes to be committed:
    	modified:   counter/index.html
    	modified:   counter/scripts/app.js
    	modified:   counter/styles/style.css

--- counter/scripts/app.js
+++ counter/scripts/app.js
@@ -1,1 +1,70 @@
-// 程式碼寫這裡
+const timer = document.querySelector(".timer");
+
+const DEFAULT_SECONDS = 120;
+let couterSec = DEFAULT_SECONDS;
+let timerId;
+let running = false;
+let pause = false;
+
+function updateTimer(sec) {
+  let mins = String(Math.floor(sec / 60)).padStart(2, "0");
+  let secs = String(sec % 60).padStart(2, "0");
+
+  timer.textContent = `${mins}:${secs}`;
+
+  if (sec === 0) {
+    timer.classList.add("times-up");
+  } else {
+    timer.classList.remove("times-up");
+  }
+}
+
+function initTimer() {
+  running = true;
+  timerId = setInterval(() => {
+    if (couterSec === 1) {
+      timesUp();
+    } else {
+      couterSec--;
+      updateTimer(couterSec);
+    }
+  }, 1000);
+}
+
+function timesUp() {
+  running = false;
+  const sound = new Audio("sounds/news.mp3");
+  sound.play();
+
+  updateTimer(0);
+  clearInterval(timerId);
+}
+
+document.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "Enter":
+      if (!running) {
+        couterSec = DEFAULT_SECONDS;
+        updateTimer(couterSec);
+        initTimer();
+      }
+      break;
+    case " ":
+      if (running) {
+        if (pause) {
+          initTimer();
+          pause = false;
+        } else {
+          clearInterval(timerId);
+          pause = true;
+        }
+      }
+      break;
+    case "Escape":
+      running = false;
+      couterSec = DEFAULT_SECONDS;
+      updateTimer(couterSec);
+      clearInterval(timerId);
+      break;
+  }
+});
